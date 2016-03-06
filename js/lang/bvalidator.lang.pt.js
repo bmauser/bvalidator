606b249c6be4dfb2707ca8cc89d419a5f9039079
Sandro Miguel Marques <sandromiguel@produlogia.com>
2016-03-06 15:55:03 +0000

    New translation: Português-Portugal (pt-PT)
    New translation to Português-Portugal (pt-PT) error messages

---
+++ js/lang/bvalidator.lang.pt.js
@@ -0,0 +1,28 @@
+/*
+ * bValidator Português-Portugal (pt-PT) error messages by sandromiguel@produlogia.com
+ */
+
+bValidator.defaultOptions.errorMessages.pt = {
+
+    'default' : 'Por favor, corrija o valor deste campo.',
+    minlen    : 'O tamanho mínimo é de {0} caracteres.',
+    maxlen    : 'O tamanho máximo é de {0} caracteres.',
+    rangelen  : 'O tamanho deve ser entre  {0} e {1} caracteres.',
+    min       : 'Por favor, insira um número maior ou igual a {0}.',
+    max       : 'Por favor, insira um número menor ou igual a {0}.',
+    between   : 'Por favor, insira um número entre {0} e {1}.',
+    required  : 'Este campo tem preenchimento obrigatório.',
+    alpha     : 'Por favor, insira apenas caracteres alfabéticos.',
+    alphanum  : 'Por favor, insira apenas caracteres alfanuméricos.',
+    digit     : 'Por favor, insira apenas dígitos.',
+    number    : 'Por favor, insira um número válido.',
+    email     : 'Por favor, insira um e-mail válido.',
+    url       : 'Por favor, insira uma URL válida.',
+    ip4       : 'Por favor, insira um endereço IPv4 válido.',
+    ip6       : 'Por favor, insira um endereço IPv6 válida.',
+    date      : 'Por favor, insira uma data válida no formato {0}',
+    equal     : 'Por favor, insira o mesmo valor novamente.',
+    differ    : 'Por favor, insira um valor diferente.'
+};
+
+bValidator.defaultOptions.lang = 'pt';
