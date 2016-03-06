d8d934e7603764ebb06c764e905b25813c4e363c
Sandro Miguel Marques <sandromiguel@produlogia.com>
2016-03-06 17:11:14 +0000

    Added files via upload

--- js/lang/bvalidator.lang.pt.js
+++ js/lang/bvalidator.lang.pt.js
@@ -2,27 +2,27 @@
  * bValidator Português-Portugal (pt-PT) error messages by sandromiguel@produlogia.com
  */
 
-bValidator.defaultOptions.errorMessages.pt = {
+bValidator.defaultOptions.messages.pt = {
 
     'default' : 'Por favor, corrija o valor deste campo.',
-    minlen    : 'O tamanho mínimo é de {0} caracteres.',
-    maxlen    : 'O tamanho máximo é de {0} caracteres.',
-    rangelen  : 'O tamanho deve ser entre  {0} e {1} caracteres.',
-    min       : 'Por favor, insira um número maior ou igual a {0}.',
-    max       : 'Por favor, insira um número menor ou igual a {0}.',
-    between   : 'Por favor, insira um número entre {0} e {1}.',
-    required  : 'Este campo tem preenchimento obrigatório.',
-    alpha     : 'Por favor, insira apenas caracteres alfabéticos.',
-    alphanum  : 'Por favor, insira apenas caracteres alfanuméricos.',
-    digit     : 'Por favor, insira apenas dígitos.',
-    number    : 'Por favor, insira um número válido.',
-    email     : 'Por favor, insira um e-mail válido.',
-    url       : 'Por favor, insira uma URL válida.',
-    ip4       : 'Por favor, insira um endereço IPv4 válido.',
-    ip6       : 'Por favor, insira um endereço IPv6 válida.',
-    date      : 'Por favor, insira uma data válida no formato {0}',
-    equal     : 'Por favor, insira o mesmo valor novamente.',
-    differ    : 'Por favor, insira um valor diferente.'
+    minlen    : 'Este campo deve ter pelo menos {0} caracteres.',
+    maxlen    : 'Este campo deve ter no máximo {0} caracteres.',
+    rangelen  : 'Este campo deve ter entre  {0} e {1} caracteres.',
+    min       : 'Por favor introduza um número maior ou igual a {0}.',
+    max       : 'Por favor introduza um número menor ou igual a {0}.',
+    between   : 'Por favor introduza um número entre {0} e {1}.',
+    required  : 'Por favor preencha este campo.',
+    alpha     : 'Por favor introduza apenas caracteres alfabéticos.',
+    alphanum  : 'Por favor introduza apenas caracteres alfanuméricos.',
+    digit     : 'Por favor introduza apenas dígitos.',
+    number    : 'Por favor introduza um número válido.',
+    email     : 'Por favor introduza um endereço de email válido.',
+    url       : 'Por favor introduza um URL válida.',
+    ip4       : 'Por favor introduza um endereço IPv4 válido.',
+    ip6       : 'Por favor introduza um endereço IPv6 válida.',
+    date      : 'Por favor introduza uma data válida no formato {0}',
+    equal     : 'Por favor introduza o mesmo valor novamente.',
+    differ    : 'Por favor introduza um valor diferente.'
 };
 
 bValidator.defaultOptions.lang = 'pt';
