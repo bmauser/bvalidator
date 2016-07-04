d1e7040da10559c60804034b540d285155b13814
bmauser <bmauser@gmail.com>
2016-07-04 02:32:50 +0200

    docs for 1.0.2

---
+++ bvalidator-dist/lang/bvalidator.lang.fr.js
@@ -0,0 +1,28 @@
+/*
+ * bValidator Français (fr) error messages by sandromiguel@produlogia.com
+ */
+
+bValidator.defaultOptions.messages.fr = {
+
+    'default' : 'Por favor, corrija o valor deste campo.',
+    minlen    : 'Ce champ doit contenir au moins {0} caractères.',
+    maxlen    : 'Ce champ doit contenir au maximum {0} caractères.',
+    rangelen  : 'Ce champ doit contenir entre {0} et {1} caractères.',
+    min       : 'Cette valeur doit être supériure ou égale à {0}.',
+    max       : 'Cette valeur doit être inférieure ou égale à {0}.',
+    between   : 'Entrer un nombre entre {0} et {1}.',
+    required  : 'Veuillez renseigner ce champ.',
+    alpha     : 'Seules les lettres sont acceptées.',
+    alphanum  : 'Utilisez des caractères alphanumériques seulement.',
+    digit     : 'Seuls les chiffres sont acceptés.',
+    number    : 'Veuillez saisir un nombre.',
+    email     : 'Veuillez entrer une adresse email valide.',
+    url       : 'Veuillez entrer une URL valide.',
+    ip4       : 'Veuillez fournir une adresse IPv4 valide.',
+    ip6       : 'Veuillez fournir une adresse IPv6 valide.',
+    date      : 'Vous devez entrer une date valide au format {0}',
+    equal     : 'Veuillez saisir la même valeur à nouveau.',
+    differ    : 'Veuillez saisir une autre valeur.'
+};
+
+bValidator.defaultOptions.lang = 'fr';
